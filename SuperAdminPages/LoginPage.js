c8f68cfcae4fcce50816059d216aed683cebd8ba
Arun24101999 <arunkumar.r@datayaan.com>
2025-10-13 14:32:34 +0530

    Login

--- SuperAdminPages/LoginPage.js
+++ SuperAdminPages/LoginPage.js
@@ -11,18 +11,36 @@ class LoginPage{
         this.ErrorMessage = page.locator('//div[@role="alert"]');
         this.UsernameAlert = page.locator('#username-helper-text');
         this.PasswordAlert = page.locator('#password-helper-text');
+
+        this.errorMessage = page.locator("//div[@class='MuiAlert-message css-1xsto0d']");
+        this.fieldErrorMessage = page.locator("#username-helper-text");
     };
  
     async LaunchUrl(URL){
         await this.page.goto(URL);
+        await this.page.waitForTimeout(1000);
         await this.LoginButton.click();
     };
  
     async enterTheCredentials(Username, Password){
         await this.UsernameField.fill(Username);
+         await this.page.waitForTimeout(1000);
         await this.PasswordField.fill(Password);
+         await this.page.waitForTimeout(1000);
         await this.SubmitButton.click();
+         await this.page.waitForTimeout(1000);
     }
+
+    async validationMessage(message){
+       const getText= await this.errorMessage.textContent();
+       await expect(getText).toContain(message);
+    }
+
+    async fieldvalidationErrorMessage(message){
+       const getText= await this.fieldErrorMessage.textContent();
+       await expect(getText).toContain(message);
+    }
+
 }
  
 module.exports={LoginPage};
